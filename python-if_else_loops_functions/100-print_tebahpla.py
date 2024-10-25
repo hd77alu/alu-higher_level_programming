e558ea5306057ef21bc1a28f2c765d9b50c34fbb
Hamed <h.abdalgade@alustudent.com>
2024-10-25 15:11:41 -0700

    100

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
-for i in range(ord('z'), ord('a') - 1, -1):
-    if (i % 2 == 0):
-        print("{}".format(chr(i)), end="")
-    else:
-        print("{}".format(chr(i - 32)), end="")
+for i in range(122, 96, -2):
+    print("{:s}{:s}".format(chr(i), chr(i - 33)), end="")
