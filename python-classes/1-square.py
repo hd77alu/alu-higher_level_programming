4f56a2608e47625ffa324701fc772ece81b33986
Hamed <h.abdalgade@alustudent.com>
2024-11-06 11:57:17 -0800

    task2

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-#class Square that defines a square by size
+"""class Square that defines a square by size"""
 
 
 class Square:
+    """empty class"""
     def __init__ (self, size):
+        """init"""
         self.__size = size
