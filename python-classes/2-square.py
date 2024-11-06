88a6b73f0c736e788773cc778eaf50a056a4e571
Hamed <h.abdalgade@alustudent.com>
2024-11-06 12:56:52 -0800

    0>size

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python
-#class Square that must be  defined by (integer) size
+"""class Square that must be  defined by (integer) size"""
 
 
 class Square:
+    """class with privte atribute"""
     def __init__(self, size=0):
         if type(size) != int:
             raise TypeError("size must be an integer")
