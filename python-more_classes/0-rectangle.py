988bc89194bdbc78584937a666a7fd1e41ed3a92
Hamed <h.abdalgade@alustudent.com>
2024-11-08 01:04:12 -0800

    first task

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This script displays an empty class Rectangle that defines a rectangle"""
+
+
+class Rectangle:
+    """ empty Rectagle class"""
+    def __init__(self):
+        """init class"""
+        pass
---
+++ python-more_classes/README.md
@@ -0,0 +1,1 @@
+This directory is for extra work on python classes
