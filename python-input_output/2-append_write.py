7ccd931be3ebf2664c13c46e8aeb7320a86c85da
Hamed <h.abdalgade@alustudent.com>
2024-11-13 14:09:35 -0800

    documentatiin

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -2,10 +2,14 @@
 """function that append text file (UTF8) and return the number of characters"""
 
 
-def apoend_write(filename="", text=""):
+def append_write(filename="", text=""):
     """
-    add apoend in text file (UTF8)
-    and returns the number of characters added
+    Appends a string to the end of a UTF8 text file.
+
+        filename: The name of the file.
+        text: The last string to append to the file.
+    Returns:
+        The number of characters that has been appended.
     """
     with open(filename, "a", encoding="utf-8") as f:
         return f.write(text)
