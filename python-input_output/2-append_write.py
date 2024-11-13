b85d22679f9ef8fa573136920115c427e11a9127
Hamed <h.abdalgade@alustudent.com>
2024-11-13 14:02:01 -0800

    3 again

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -3,6 +3,7 @@
 
 
 def apoend_write(filename="", text=""):
-    """add apoend in text file (UTF8)"""
+    """add apoend in text file (UTF8)
+    and returns the number of characters added"""
     with open(filename, "a", encoding="utf-8") as f:
         return f.write(text)
