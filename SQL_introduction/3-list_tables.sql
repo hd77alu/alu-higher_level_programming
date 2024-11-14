db05679494d975c9c3e505d9e802d18b8ece3aa1
Hamed <h.abdalgade@alustudent.com>
2024-11-14 13:11:04 -0800

    till 10

---
+++ SQL_introduction/3-list_tables.sql
@@ -0,0 +1,2 @@
+-- a script that lists all tables of a database on my MySQL server.
+SHOW TABLES;
---
+++ SQL_introduction/6-list_values.sql
@@ -0,0 +1,2 @@
+-- a scipt that lists all rows of the table first_table.
+SELECT * FROM `first_table`;
