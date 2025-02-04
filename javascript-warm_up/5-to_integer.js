bbf7fb5a52356af8bab335afc8204524efdc2bbd
Hamed <h.abdalgade@alustudent.com>
2025-02-04 06:43:20 -0800

    semistand8

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
 if (!isNaN(process.argv[2])) {
-console.log('My number: ' + process.argv[2]);
+  console.log('My number: ' + process.argv[2]);
 } else {
-console.log('Not a number');}
+  console.log('Not a number');
+}
