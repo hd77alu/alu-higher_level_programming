fbfc26f7157fc3bd6dfbc104c4d65cdb5d99805c
Hamed <h.abdalgade@alustudent.com>
2025-02-04 07:01:59 -0800

    done

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -4,7 +4,7 @@ if ((process.argv.length === 2) || (process.argv.length === 3)) {
   console.log(0);
 } else {
   for (let i = 2; i < process.argv.length; i++) {
-  arr.push(parseInt(process.argv[i]));
+    arr.push(parseInt(process.argv[i]));
   }
   const max = Math.max(...arr);
   arr.splice(arr.indexOf(max), 1);
