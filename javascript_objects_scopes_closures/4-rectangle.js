346e9af9946fa8ff38e11ddfb4eec213c00f66aa
Hamed <h.abdalgade@alustudent.com>
2025-02-05 23:46:42 -0800

    done

--- javascript_objects_scopes_closures/4-rectangle.js
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -5,23 +5,25 @@ class Rectangle {
   if (w > 0 && h > 0) {
   this.width = w;
   this.height = h;
-}
-}
+			          }
+		    }
 
   print () {
   let i = 0;
-  for (i; i < this.height; i++);
+  for (i; i < this.height; i++) {
   console.log('X'.repeat(this.width));
-}
-}
+			          }
+		    }
+
   rotate () {
   const tmp = this.width;
   this.width = this.height;
   this.height = tmp;
-}
+		    }
+
   double () {
   this.width *= 2;
   this.height *= 2;
-}
+		    }
 }
 module.exports = Rectangle;
